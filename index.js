dfacfea55be9cabea5d837e69560274518103824
birinder singh <birinder229.be22@chitkara.edu.in>
2025-07-26 22:11:56 +0530

    Registration route done

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import cors from 'cors';
+import userRoutes from './routes/userRoutes.js';
+
 
 dotenv.config();
 const app = express();
@@ -15,6 +17,8 @@ connectDB();
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
+
+app.use('/api/users', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);

ce5d402fa5a845e9f9ee9deaf842e256b04bb65e
birinder singh <birinder229.be22@chitkara.edu.in>
2025-07-26 22:02:51 +0530

    mongoDB setup Done

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+import connectDB from './config/db.js';
 import cors from 'cors';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.get('/ping', (req, res) => {
-  res.json({ message: 'RentHub backend is alive!' });
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
 });
 
-// MongoDB Connection
-mongoose.connect(process.env.MONGO_URI).then(() => {
-  console.log('✅ Connected to MongoDB');
-  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-}).catch(err => {
-  console.error('❌ MongoDB connection failed:', err.message);
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });

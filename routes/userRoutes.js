e13c186422cd131c01d9eaae013c3d66d5134b34
birinder singh <birinder229.be22@chitkara.edu.in>
2025-07-26 23:45:13 +0530

    routes added for login

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { registerUser } from '../controllers/userController.js';
+import { registerUser, loginUser } from '../controllers/userController.js';
 import { verifyUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
+router.post('/login', loginUser);
 router.post('/verify', verifyUser);
 
 export default router;

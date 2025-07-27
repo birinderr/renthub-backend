8efb4323f4995331a63866a6214fe09b14c7bb40
birinder singh <birinder229.be22@chitkara.edu.in>
2025-07-27 22:50:37 +0530

    added routes for approval and rejection booking

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createBooking, getMyBookings, getOwnerBookings } from '../controllers/bookingController.js';
+import { createBooking, getMyBookings, getOwnerBookings, updateBookingStatus } from '../controllers/bookingController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -9,6 +9,6 @@ router.post('/', protect, createBooking);
 
 router.get('/my', protect, getMyBookings);
 router.get('/owner', protect, getOwnerBookings);
-
+router.put('/:id/status', protect, updateBookingStatus);
 
 export default router;

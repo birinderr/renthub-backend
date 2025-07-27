9cab111cbbb45462b68cf661528009eece7a75cf
birinder singh <birinder229.be22@chitkara.edu.in>
2025-07-27 19:11:33 +0530

    updated admin routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllUsers, deleteUser , updateUserByAdmin} from '../controllers/adminController.js';
+import { getAllUsers, deleteUser , updateUserByAdmin, approveItem} from '../controllers/adminController.js';
 import { protect, isAdmin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/users', protect, isAdmin, getAllUsers);
 router.put('/user/:id', protect, isAdmin, updateUserByAdmin);
 router.delete('/user/:id', protect, isAdmin, deleteUser);
+router.put('/items/:id/approve', protect, isAdmin, approveItem);
 
 export default router;

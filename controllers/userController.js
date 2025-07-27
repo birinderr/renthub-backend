56c4be797a08ea5c98bf8079ad305b11acd369c5
birinder singh <birinder229.be22@chitkara.edu.in>
2025-07-27 14:33:53 +0530

    made a update profile controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -83,3 +83,30 @@ export const getUserProfile = async (req, res) => {
     res.status(500).json({ message: 'Server error', error: error.message });
   }
 };
+
+export const updateUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10); // creating random string
+      user.password = await bcrypt.hash(req.body.password, salt); // adding the random string to the hashed password
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};

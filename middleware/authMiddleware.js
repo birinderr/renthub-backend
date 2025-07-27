8becc04fa8ad0d702bb24f214faba212325725cc
birinder singh <birinder229.be22@chitkara.edu.in>
2025-07-27 15:17:20 +0530

    made a middleware to check admin

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -23,3 +23,11 @@ export const protect = async (req, res, next) => {
     res.status(401).json({ message: 'Not authorized, no token' });
   }
 };
+
+export const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next(); 
+  } else {
+    res.status(403).json({ message: 'Not authorized as admin' });
+  }
+};

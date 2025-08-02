137648449f093c6f349c206ddf46e7ed9f0a000b
birinder singh <birinder229.be22@chitkara.edu.in>
2025-08-03 02:55:17 +0530

    minor cors changes

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://renthub-frontend-rosy.vercel.app/', 
+  credentials: true
+}));
 app.use(express.json());
 
 connectDB();
